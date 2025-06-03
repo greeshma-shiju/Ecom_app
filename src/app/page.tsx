3ebd7fff602829ea6153a1dca6a13159e8c309b9
Greeshma <1986greeshma@gmail.com>
2025-06-03 08:55:24 +0400

    added images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,18 @@ export default function Home() {
   return (
     <div>
      <h1>This is home page</h1>
+     <div>
      <Image src={'/images/bird.jpeg'} alt='Sample image' width={300} height={300}/>
+     </div>
+     <div>
+     <Image src={'/images/bird.jpeg'} alt='Sample image' width={300} height={300}/>
+     </div>
+     <div>
+     <Image src={'/images/bird.jpeg'} alt='Sample image' width={300} height={300}/>
+     </div>
+     <h3>
+      This is home page contents
+     </h3>
      </div>
   )
 }
